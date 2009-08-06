8cd8ca057c3038b10b7028073731048af9e47eb5
vyk2rr <vyk2rr@5b89ad25-372c-0410-87af-59d4b192d990>
2009-08-06 06:42:34 +0000

    first part of public gelatocms in flavored version.

---
+++ branches/flavored/config-sample.php
@@ -0,0 +1,10 @@
+<?php
+define('requiresBD', true);
+define('DB_Engine', 'mysqli');
+define('DB_Server', 'localhost');
+define('DB_name', '');
+define('DB_User', '');
+define('DB_Password', '');
+define('DB_Port', false);
+define('Path', "http://localhost");
+?>
